79f4dc4e79b5817c2d990fc9693adafb46c858c1
Rebien <k.rebien@gmail.com>
2020-11-22 16:13:59 +0100

    setup reducers and store

---
+++ src/store/reducers/authReducer.js
@@ -0,0 +1,8 @@
+const initState = {}
+
+//Create a piece of store and handle the query for all of auths
+const authStore = (state = initState, action) =>{
+    return state
+}
+
+export default authStore
---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,13 @@
+const initState = {
+    projects: [
+        {id: '1', title: 'BOMBO LCAT', content: 'You know what the ksay is chirping in the dis'},
+        {id: '2', title: 'makein the brad in task', content: 'You know what the ksay is chirping in the dis'},
+        {id: '3', title: 'Mah makein  in task', content: 'You know what the ksay is chirping in the dis'}
+    ]
+}
+
+const projectReducer = (state = initState, action) =>{
+    return state
+}
+
+export default projectReducer
